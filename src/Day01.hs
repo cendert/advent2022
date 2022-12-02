848f3504290d3eead42609babece47c0c9d334f7
= <=>
2022-12-02 01:09:29 +0100

    day01a completed and downgraded ghc version

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,22 @@
 module Day01 where
 
+addIfNotBlank :: [String] -> String -> [String]
+addIfNotBlank [] curr = [curr] 
+addIfNotBlank (x:xs) curr
+  | curr == "" = "0":x:xs
+  | otherwise = show ((read curr :: Int) + (read x :: Int)):xs
+
+keepBiggest :: Int -> String -> Int
+keepBiggest prev curr
+  | prev < (read curr :: Int) = read curr :: Int 
+  | otherwise = prev
+
 day01 :: IO ()
 day01 = do
     content <- readFile "src/day01.input.txt"
     let contentLines = lines content
-    print contentLines
+    let subTotals = foldl addIfNotBlank [] contentLines
+    let maxValue = foldl keepBiggest 0 subTotals
+    print maxValue
+
     return ()
-
-
