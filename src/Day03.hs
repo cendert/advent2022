62de004e10d6a57f3ada980e26ba296f9fa6122c
= <=>
2022-12-05 13:00:50 +0100

    day03 complete

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,10 +1,18 @@
 module Day03 where
+import Text.Printf (printf)
 
 splitToEvenParts :: [a] -> ([a], [a])
 splitToEvenParts xs = splitAt (length xs `div` 2) xs
 
+toThruple :: [String] -> [(String, String, String)]
+toThruple [] = []
+toThruple (x1:x2:x3:xs) = (x1,x2,x3) : toThruple xs
+
 findDuplicate :: Eq a => ([a], [a]) -> a
 findDuplicate (xs, ys) = head [x | x <- xs, y <- ys, x == y]
+
+findCommonValueInThruple :: Eq a => ([a], [a], [a]) -> a
+findCommonValueInThruple (xs, ys, zs) = head [ x | x <-xs, y <- ys, x == y, z <- zs, x == z]
 
 lowerCaseIndexes :: [(Char, Integer)]
 lowerCaseIndexes = zip ['a'..'z'] [1..26]
@@ -27,10 +35,14 @@ day03 = do
     let contentLines = lines content
     let evenParts = map splitToEvenParts contentLines
     let duplicates = map findDuplicate evenParts
-    -- print duplicates
     let prioValues = map (getCharIndex charIndexMapping) duplicates
     let prioTotal = sum prioValues
-    -- print prioValues
-    print prioTotal
+    printf "The total prio based on the duplicate values within the rucksacks is %i\n" prioTotal
+
+    let perGroupOfThree = toThruple contentLines
+    let commonValues = map findCommonValueInThruple perGroupOfThree
+    let prioValuesOfGroupBadges = map (getCharIndex charIndexMapping) commonValues
+    let prioTotalOfGroupBadges = sum prioValuesOfGroupBadges
+    printf "The total prio based on the common values within three rucksacks is %i\n" prioTotalOfGroupBadges
     
     return ()
