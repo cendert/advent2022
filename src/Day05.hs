363cc3d4a12e96f68f71b694fd45d361769c99c5
= <=>
2022-12-09 00:50:59 +0100

    fix

--- src/Day05.hs
+++ src/Day05.hs
@@ -5,8 +5,8 @@ import Data.List (transpose)
 toIndexCharTuples :: [Char] -> [(Int, Char)]
 toIndexCharTuples = foldl (\acc curr -> acc ++ [(length acc, curr)]) []
 
-getCharIndexes :: [String] -> [Int]
-getCharIndexes xs = take (length xs + 2 // 4) [ 4 * x - 3 | x <- [1..]]
+getCharIndexes :: [[(Int,Char)]] -> [Int]
+getCharIndexes xs = take (length xs + 2 `div` 4) [ 4 * x - 3 | x <- [1..]]
 
 toStacks :: [Int] -> [(Int, Char)] -> [Char]
 toStacks indexes xs = [ snd x | x <- xs, i <- indexes, fst x == i]
@@ -17,16 +17,10 @@ day05 = do
     -- content <- readFile "src/day05.input.txt"
     let contentLines = lines content
     let stackLines = lines $ init $ takeWhile (not . isDigit) content
-    print stackLines
     let stackTuplesPerLine = map toIndexCharTuples stackLines
-    print stackTuplesPerLine
-    -- wanted indexes are 1, 5, 9
-    let possibleValues = take 3 [ 4 * x - 3 | x <- [1..]]
-    print possibleValues
-    let actualChars = map (toStacks possibleValues) stackTuplesPerLine 
-    print actualChars
+    let charIndexes = getCharIndexes stackTuplesPerLine
+    let actualChars = map (toStacks charIndexes) stackTuplesPerLine
     let perStack = transpose actualChars
-    print perStack
     let withoutSpaces = filter isAlpha <$> perStack
     print withoutSpaces
 
